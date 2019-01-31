1d387537ac92fab4951d9db37efa4c6f5aa0e6ab
Manuel camacho <maedca@gmail.com>
2019-01-30 22:58:47 -0400

    Relationships fixed

--- app/ConferenceSchedule.php
+++ app/ConferenceSchedule.php
@@ -6,10 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ConferenceSchedule extends Model
 {
-    protected $fillable = ['industry_id','conferencia_id', 'empresa_id','relacionista_id', 'course_date', 'morning_assistant', 'afternoon_assistant', 'night_assistant', 'night_assistant'];
+    protected $fillable = ['industry_id', 'conferencia_id', 'empresa_id', 'relacionista_id', 'course_date', 'morning_assistant', 'afternoon_assistant', 'night_assistant', 'night_assistant'];
 
-    public function conference()
+    public function conferencia()
     {
         return $this->belongsTo('App\Conferencia');
     }
+
+    public function empresa()
+    {
+        return $this->belongsTo('App\Empresa');
+    }
+
 }
--- app/Empresa.php
+++ app/Empresa.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Empresa extends Model
 {
-    protected $fillable = ['name','nit', 'url', 'industria', 'empleados', 'tel_ofi', 'cel', 'contacto_1','cargo_1', 'tel_1','contacto_2', 'cargo_2','tel_2','descripcion','email','calle','ciudad', 'municipio','barrio','pais'];
+    protected $fillable = ['name', 'nit', 'url', 'industria', 'empleados', 'tel_ofi', 'cel', 'contacto_1', 'cargo_1', 'tel_1', 'contacto_2', 'cargo_2', 'tel_2', 'descripcion', 'email', 'calle', 'ciudad', 'municipio', 'barrio', 'pais'];
 
     public function relacionista()
     {
-        return $this->belongsTo('App\Realacionista');
+        //return $this->belongsTo('App\Realacionista');
+        return $this->belongsTo('App\Relacionista');
     }
 }
