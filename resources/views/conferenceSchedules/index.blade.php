e28dab7d59b8e5c68af10fba2c61d3a9ab684c63
Manuel camacho <maedca@gmail.com>
2019-01-30 23:01:24 -0400

    index fixed

--- resources/views/conferenceSchedules/index.blade.php
+++ resources/views/conferenceSchedules/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.master')
 @section('content')
-    <div class="content mt-4">
-        <div class="container-fluid">
+<div class="content mt-4">
+    <div class="container-fluid">
 
-            <table class="table table-hover" id="table" >
-                <thead>
+        <table class="table table-hover" id="table">
+            <thead>
 
                 <tr>
                     <th>Conferencia Agendad</th>
@@ -12,45 +12,46 @@
                     <th>Acciones</th>
                 </tr>
 
-                </thead>
-                <tbody>
+            </thead>
+            <tbody>
                 @foreach( $conferenceSchedules as $conferenceSchedule)
-                    <tr>
+                <tr>
 
-                        <td>{{$conferenceSchedule->conference()->get()->name}}</td>
+                    <td>{{$conferenceSchedule->conferencia()->first()->name}}</td>
 
 
-                        <td>
-                            <form action="{{route('conferencias.destroy', $conferencia)}}" method="post">
-                                @csrf
-                                @method('delete')
-                                <a href="{{route('conferencias.edit', $conferencia)}}"> <i class="fa fa-edit"></i></a>
-                                <button class="btn" type="submit"><i class="fa fa-trash" style="color:red"></i></button>
-                                {{--<input type="submit" value="Eliminar" class="btn btn-link form-inline">--}}
-                            </form></td>
-                    </tr>
+                    <td>
+                        <form action="{{route('conferencias.destroy', $conferenceSchedule)}}" method="post">
+                            @csrf
+                            @method('delete')
+                            <a href="{{route('conferencias.edit', $conferenceSchedule)}}"> <i class="fa fa-edit"></i></a>
+                            <button class="btn" type="submit"><i class="fa fa-trash" style="color:red"></i></button>
+                            {{--<input type="submit" value="Eliminar" class="btn btn-link form-inline">--}}
+                        </form>
+                    </td>
+                </tr>
                 @endforeach
-                </tbody>
-            </table>
-        </div>
+            </tbody>
+        </table>
     </div>
+</div>
 
 @endsection
 
 @section('styles')
-    <link href="{{ asset('css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
 
 @endsection
 
 @section('scripts')
-    <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('js/dataTables.bootstrap4.min.js') }}"></script>
-    <script>
-        $(document).ready(function () {
-            $('#table').DataTable();
+<script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+<script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('js/dataTables.bootstrap4.min.js') }}"></script>
+<script>
+    $(document).ready(function () {
+        $('#table').DataTable();
 
-        });
-    </script>
+    });
+
+</script>
 @endsection
-
